83907f96e237bd2184bc3015a6c0733cdbdbc577
HYUN TAE YUN <yoonrt369@naver.com>
2021-11-28 14:36:19 +0900

    Initial commit

---
+++ src/main/java/com/petdamall/petdamall2022/Petdamall2022Application.java
@@ -0,0 +1,13 @@
+package com.petdamall.petdamall2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Petdamall2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Petdamall2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/petdamall/petdamall2022/Petdamall2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petdamall.petdamall2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Petdamall2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
